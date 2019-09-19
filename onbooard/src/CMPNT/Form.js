d228806979bf2d7ab37da446ef680f5ee81d82b8
CScori <amilanotte1366@gmail.com>
2019-09-19 14:34:42 -0500

    adds fields for form fleshed out

--- onbooard/src/CMPNT/Form.js
+++ onbooard/src/CMPNT/Form.js
@@ -17,16 +17,26 @@ const Form = (values, errors, touched, status) => {
         <div>
             <Form>
             <FormGroup>
-                    <Label htmlFor="name">Name</Label>
+                    <Label htmlFor="name">Name
+                    <Field type="text" name="name" placeholder="First and Last Name" />
+                    </Label>
+                </FormGroup>
+                {/* sex drop down */}
+                {/* birthdate calendar */}
+                <FormGroup>
+                    <Label htmlFor="email">Email
+                    <Field type="email" name="email" placeholder="Enter Personal Email" />
+                    </Label>
                 </FormGroup>
                 <FormGroup>
-                    <Label htmlFor="email">Email</Label>
+                    <Label htmlFor="password">Password
+                    <Field type="text" name="password" placeholder="Please Create a Unique Password" />
+                    </Label>
                 </FormGroup>
                 <FormGroup>
-                    <Label htmlFor="password">Password</Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="check">Terms of Service</Label>
+                    <Label htmlFor="Terms"> Terms of Service
+                    <Field type="checkbox" name="terms">I accept and agree to Terms of Service</Field>
+                    </Label>
                 </FormGroup>
                 
                 <Button>Submit</Button>
@@ -37,7 +47,7 @@ const Form = (values, errors, touched, status) => {
 }
 
 const FormikForm = withFormik({
- mapPropsToValues({ name, email, password, check })  {
+ mapPropsToValues({ name, email, password, terms })  {
      return {
          name: name || "",
          email: email || "",
